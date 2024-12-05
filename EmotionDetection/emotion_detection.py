0c6e5e728220d4a715e7011a77b79e5874f33597
John Leahy <john.leahy.career@gmail.com>
2024-12-05 04:54:58 -0500

    added server module

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -6,7 +6,20 @@ def emotion_detector(text_to_analyse):
     header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     obj = { "raw_document": { "text": text_to_analyse } }
 
+    result={
+    'anger':   None,
+    'disgust': None,
+    'fear':    None,
+    'joy':     None,
+    'sadness': None,
+    "dominant_emotion": None
+    }
+
     response = requests.post(url, json=obj, headers=header)
+    status = response.status_code
+    if status == 400:
+        return result
+
     formatted = json.loads(response.text)
     emotions = formatted['emotionPredictions'][0]['emotion']
 
@@ -19,17 +32,11 @@ def emotion_detector(text_to_analyse):
     dominant_emotion = ''
     top_score = 0
     for item in emotions.items():
+        result[item[0]] = item[1]
         if item[1] > top_score:
             top_score = item[1]
             dominant_emotion = item[0]
-
-    result={
-    'anger':   anger_score,
-    'disgust': disgust_score,
-    'fear':    fear_score,
-    'joy':     joy_score,
-    'sadness': sadness_score,
-    "dominant_emotion": dominant_emotion
-    }
+    
+    result['dominant_emotion'] = dominant_emotion
 
     return result
